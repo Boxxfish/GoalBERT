b276ba74fc4a79825186b77fc58ab1df4accecaa
Ben Giacalone <bensgiacalone@gmail.com>
2024-03-12 13:30:48 -0400

    Implement metrics (P, R, F1, EM)

---
+++ eval/metrics.py
@@ -0,0 +1,36 @@
+"""
+Common retrieval metrics.
+Each metric expects a set of predicted results, and a set of actual results.
+"""
+
+from typing import *
+
+T = TypeVar("T")
+
+def precision(pred: Set[T], actual: Set[T]) -> float:
+    """
+    What percent of the predictions are in the set of actual items.
+    """
+    tp = pred.intersection(actual)
+    return len(tp) / len(pred)
+
+def recall(pred: Set[T], actual: Set[T]) -> float:
+    """
+    What percent of the actual items are in the predicted set.
+    """
+    tp = pred.intersection(actual)
+    return len(tp) / len(actual)
+
+def f1(pred: Set[T], actual: Set[T]) -> float:
+    """
+    Balances both precision and recall.
+    """
+    p = precision(pred, actual)
+    r = recall(pred, actual)
+    return (2 * p * r) / (p + r)
+
+def em(pred: Set[T], actual: Set[T]) -> float:
+    """
+    This is a 1 for exact matches, and 0 otherwise.
+    """
+    return pred == actual
---
+++ eval/test_metrics.py
@@ -0,0 +1,55 @@
+from typing import *
+
+from eval.metrics import *
+
+def test_precision():
+    pred = {1, 2, 3, 4, 5}
+    actual = {1, 2, 3, 4, 5}
+    assert precision(pred, actual) == 1
+
+    pred = {1}
+    actual = {1, 2, 3, 4, 5}
+    assert precision(pred, actual) == 1
+
+    pred = {1, 2, 3, 4, 5}
+    actual = {1}
+    assert precision(pred, actual) == 1 / 5
+
+def test_recall():
+    pred = {1, 2, 3, 4, 5}
+    actual = {1, 2, 3, 4, 5}
+    assert recall(pred, actual) == 1
+
+    pred = {1}
+    actual = {1, 2, 3, 4, 5}
+    assert recall(pred, actual) == 1 / 5
+
+    pred = {1, 2, 3, 4, 5}
+    actual = {1}
+    assert recall(pred, actual) == 1
+
+def test_f1():
+    pred = {1, 2, 3, 4, 5}
+    actual = {1, 2, 3, 4, 5}
+    assert f1(pred, actual) == 1
+
+    pred = {1}
+    actual = {1, 2, 3, 4, 5}
+    assert f1(pred, actual) == (2 * 1 * (1 / 5)) / (1 + (1 / 5))
+
+    pred = {1, 2, 3, 4, 5}
+    actual = {1}
+    assert f1(pred, actual) == (2 * (1 / 5) * 1) / ((1 / 5) + 1)
+
+def test_em():
+    pred = {1, 2, 3, 4, 5}
+    actual = {1, 2, 3, 4, 5}
+    assert em(pred, actual) == 1
+
+    pred = {1}
+    actual = {1, 2, 3, 4, 5}
+    assert em(pred, actual) == 0
+
+    pred = {1, 2, 3, 4, 5}
+    actual = {1}
+    assert em(pred, actual) == 0
