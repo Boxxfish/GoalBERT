f8e5181a7f9b7f22a86642a07b3a53e34b130809
Ben Giacalone <bensgiacalone@gmail.com>
2024-03-17 23:15:24 -0400

    More wrangling

---
+++ index.py
@@ -0,0 +1,23 @@
+"""
+Indexing script for GoalBERT.
+Before training or evaluating, an index must be generated.
+"""
+
+from colbert.infra import Run, RunConfig, ColBERTConfig
+from colbert import Indexer
+
+
+def main():
+    with Run().context(RunConfig(nranks=1, experiment="wiki2017")):
+        config = ColBERTConfig(
+            nbits=2,
+            root="./index",
+        )
+        indexer = Indexer(checkpoint="../colbertv2.0", config=config)
+        indexer.index(
+            name="wiki2017.nbits=2", collection="../fact_collection.tsv"
+        )
+
+
+if __name__ == "__main__":
+    main()
---
+++ test.c
@@ -0,0 +1,5 @@
+#include <x86_64-linux-gnu/python3.8/pyconfig.h>
+
+int main() {
+	return 0;
+}
