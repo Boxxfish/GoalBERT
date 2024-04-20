Fix masking issues and use multiple GPUs

goalbert/training/goalbert.py
@@ -105,7 +105,7 @@ class GoalBERT(ColBERT):
             non_masks_padded[: non_masks.shape[0], :] = non_masks
 
             # Compute action masks
-            action_masks = torch.ones((MAX_MASKS, MAX_ACTIONS), dtype=torch.long)
+            action_masks = torch.ones((MAX_MASKS, MAX_ACTIONS), dtype=torch.int)
             action_masks[: interaction.shape[0], : interaction.shape[1]] = 0
 
             probs_all.append(probs)

goalbert/training/ppo.py
@@ -35,9 +35,6 @@ def train_ppo(
     p_net.train()
     v_net_frozen = copy.deepcopy(v_net)
     v_net.train()
-    if device.type != "cpu":
-        p_net.to(device)
-        v_net.to(device)
 
     total_v_loss = 0.0
     total_p_loss = 0.0
@@ -67,18 +64,17 @@ def train_ppo(
             returns = returns.to(device=device)
             advantages = advantages.to(device=device)
             action_masks = action_masks.to(device=device)
-
-            action_probs[action_masks] = 0
             
             # Train policy network
             with torch.no_grad():
                 old_act_probs = torch.gather(action_probs, 2, actions[..., None]).squeeze(-1).log()
+                old_act_probs[action_masks[:, :, 0]] = 0
                 old_act_probs = old_act_probs.sum(-1)
             new_act_probs, _, _ = p_net.compute_probs(
                 prev_input_ids, prev_attn_masks
             )
-            new_act_probs[action_masks] = 0
             new_act_probs = torch.gather(new_act_probs, 2, actions[..., None]).squeeze(-1).log()
+            new_act_probs[action_masks[:, :, 0]] = 0
             new_act_probs = new_act_probs.sum(-1)
             term1 = (new_act_probs - old_act_probs).exp() * advantages.squeeze()
             term2 = (1.0 + epsilon * advantages.squeeze().sign()) * advantages.squeeze()
@@ -87,7 +83,7 @@ def train_ppo(
             total_p_loss += p_loss.item()
 
             # Train value network
-            diff = v_net(prev_input_ids, prev_attn_masks) - returns
+            diff = v_net(prev_input_ids.to("cuda:1"), prev_attn_masks.to("cuda:1")) - returns.to("cuda:1")
             v_loss = (diff * diff).mean() / gradient_steps
             v_loss.backward()
             total_v_loss += v_loss.item()
@@ -98,9 +94,6 @@ def train_ppo(
                 p_opt.zero_grad()
                 v_opt.zero_grad()
 
-    if device.type != "cpu":
-        p_net.cpu()
-        v_net.cpu()
     p_net.eval()
     v_net.eval()
 

goalbert/training/rollout_buffer.py
@@ -67,7 +67,7 @@ class RolloutBuffer:
         rewards: List[float],
         dones: List[bool],
         truncs: List[bool],
-        masks: Optional[List[int]] = None,
+        masks: torch.Tensor,
     ):
         """
         Inserts a transition from each environment into the buffer. Make sure
@@ -90,10 +90,7 @@ class RolloutBuffer:
             self.truncs[self.next].copy_(
                 torch.tensor(truncs, dtype=torch.float, device=d)
             )
-            if masks:
-                self.masks[self.next].copy_(
-                    torch.tensor(masks, dtype=torch.bool, device=d)
-                )
+            self.masks[self.next].copy_(masks)
 
         self.next += 1
 
@@ -124,7 +121,7 @@ class RolloutBuffer:
         advantages, and dones.
         """
         with torch.no_grad():
-            d = torch.device("cpu")
+            d = torch.device("cuda:1")
             returns = torch.zeros(
                 [self.num_steps, self.num_envs], dtype=torch.float, device=d
             )
@@ -132,18 +129,18 @@ class RolloutBuffer:
                 [self.num_steps, self.num_envs], dtype=torch.float, device=d
             )
             step_returns: torch.Tensor = v_net(
-                self.input_ids[self.next], self.attn_masks[self.next]
+                self.input_ids[self.next].to(d), self.attn_masks[self.next].to(d)
             ).squeeze()
 
             # Calculate advantage estimates and rewards to go
             state_values = step_returns.clone()
             step_advantages = torch.zeros([self.num_envs], dtype=torch.float, device=d)
             for i in reversed(range(self.num_steps)):
-                prev_input_ids = self.input_ids[i]
-                prev_attn_masks = self.attn_masks[i]
-                rewards = self.rewards[i]
-                inv_dones = 1.0 - self.dones[i]
-                inv_truncs = 1.0 - self.truncs[i]
+                prev_input_ids = self.input_ids[i].to(d)
+                prev_attn_masks = self.attn_masks[i].to(d)
+                rewards = self.rewards[i].to(d)
+                inv_dones = 1.0 - self.dones[i].to(d)
+                inv_truncs = 1.0 - self.truncs[i].to(d)
                 prev_state_values: torch.Tensor = v_net(
                     prev_input_ids, prev_attn_masks
                 ).squeeze()
@@ -164,16 +161,16 @@ class RolloutBuffer:
 
             # Permute transitions to decorrelate them
             exp_count = self.num_envs * self.num_steps
-            indices = torch.randperm(exp_count, dtype=torch.int, device=d)
-            rand_prev_input_ids = self.input_ids.flatten(0, 1).index_select(0, indices)
+            indices = torch.randperm(exp_count, dtype=torch.int)
+            rand_prev_input_ids = self.input_ids.flatten(0, 1).index_select(0, indices).to(d)
             rand_prev_attn_masks = self.attn_masks.flatten(0, 1).index_select(
                 0, indices
-            )
-            rand_actions = self.actions.flatten(0, 1).index_select(0, indices)
-            rand_action_probs = self.action_probs.flatten(0, 1).index_select(0, indices)
-            rand_masks = self.masks.flatten(0, 1).index_select(0, indices)
-            rand_returns = returns.flatten(0, 1).index_select(0, indices)
-            rand_advantages = advantages.flatten(0, 1).index_select(0, indices)
+            ).to(d)
+            rand_actions = self.actions.flatten(0, 1).index_select(0, indices).to(d)
+            rand_action_probs = self.action_probs.flatten(0, 1).index_select(0, indices).to(d)
+            rand_masks = self.masks.flatten(0, 1).index_select(0, indices).to(d)
+            rand_returns = returns.flatten(0, 1).index_select(0, indices.to(d))
+            rand_advantages = advantages.flatten(0, 1).index_select(0, indices.to(d))
             batch_count = exp_count // batch_size
             batches = []
             for i in range(batch_count):